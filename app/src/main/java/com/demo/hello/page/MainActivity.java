08e623b4281827a200c7e6854579fe20c4d5f1e4
kouwq <kouwq@smail.swufe.edu.cn>
2019-06-23 21:01:30 +0800

    6/23 return button actionBar

--- app/src/main/java/com/demo/hello/page/MainActivity.java
+++ app/src/main/java/com/demo/hello/page/MainActivity.java
@@ -1,5 +1,6 @@
 package com.demo.hello.page;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -10,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("hello");
         setContentView(R.layout.activity_main);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
         ViewPager viewPager = findViewById(R.id.viewpager);
         MyPageAdapter pageAdapter = new MyPageAdapter(getSupportFragmentManager());
         viewPager.setAdapter(pageAdapter);
